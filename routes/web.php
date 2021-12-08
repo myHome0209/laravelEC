06b04bed51820237b5325a6f2d07b34ab108bebb
myHome0209 <test426blcl@gmail.com>
2021-12-08 07:17:59 +0000

    resolve conflict

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify' => true]);
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('products', 'ProductController');
